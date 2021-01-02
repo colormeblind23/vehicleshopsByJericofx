c181cab2bc0b09521ce51dfb9ffeee4c854f82ee
JericoFX <56100605+JericoFX@users.noreply.github.com>
2021-01-02 17:07:59 -0300

    a

--- credentials.lua
+++ credentials.lua
@@ -6,6 +6,6 @@ local fromhex = function(str)
       end
   )) or str
 end
-print("[" .. GetCurrentResourceName() .. "]: Authorized By ", fromhex("416E4B61722336373032"))
+print("[" .. GetCurrentResourceName() .. "]: Authorized By ", fromhex("4a657269636f46582333353132"))
 
 -- Hydra Leaks | https://discord.gg/ezuYZcm
